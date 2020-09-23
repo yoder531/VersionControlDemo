299f27b0338f5b0efc452cf9d00a62720c0a889d
dyoder <dyoder@westga.edu>
2020-09-23 19:38:24 -0400

    Adding more properties.

--- VersionControlDemo/Model/Student.cs
+++ VersionControlDemo/Model/Student.cs
@@ -9,5 +9,8 @@ namespace VersionControlDemo.Model
     public class Student
     {
         public string FirstName { get; set; }
+        public string  LastName { get; set; }
+        public int Grade { get; set; }
+
     }
 }
